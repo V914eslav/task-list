1762a897fc8ee3cdb501ce6775583785029b58c6
V914eslav <v914-working@rambler.ru>
2022-03-12 16:38:34 +0300

    create components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("123");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  );
+};
+const AppHeader = () => {
+  return <h1>My TODO List</h1>;
+};
+const SearchPanel = () => {
+  return <input placholder="search" />;
+};
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
